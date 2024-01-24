2793f195b4b3466df95cecaddace3e5b70d63787
MohammedHassan07 <mhassancs260@gmail.com>
2024-01-24 01:48:29 -0800

    feat - implemented timer

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,1 +1,16 @@
 console.log('home.js')
+
+
+let time = document.getElementById('time');
+let counter = parseInt(time.innerText);
+
+const countdown = setInterval(() => {
+    counter--;
+    time.innerText = counter;
+  
+    if (counter === 0) {
+        counter = 59
+
+        // make request
+    }
+}, 1000);
